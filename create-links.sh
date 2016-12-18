0a08c82dcce89a1ff5da61366b689b3c6aec8361
Stuart White <stubillwhite@gmail.com>
2016-12-18 08:25:53 +0000

    Update setup script

--- create-links.sh
+++ create-links.sh
@@ -3,5 +3,6 @@ setopt EXTENDED_GLOB
 for FILE in runcoms/^README.md; do
   HOME_FILE="$HOME/.`basename $FILE`"
   rm -f $HOME_FILE 2> /dev/null || true
-  ##ln -s $FILE $HOME_FILE
+  ln -s "$(pwd)/$FILE" $HOME_FILE
 done
+ln -s "$(pwd)" ~/.zprezto
