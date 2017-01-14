da16f8f89621693ec0febef4c1a72fed0b73cf6e
Stuart White <s.white.1@elsevier.com>
2017-01-14 12:02:01 +0000

    Improvements to boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 setopt EXTENDED_GLOB
+realpath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 for FILE in runcoms/^README.md; do
   HOME_FILE="$HOME/.`basename $FILE`"
   rm -f $HOME_FILE 2> /dev/null || true
-  ln -s "$(pwd)/$FILE" $HOME_FILE
+  ln -s `realpath $FILE` $HOME_FILE
 done
-ln -s "$(pwd)" ~/.zprezto
+
+ln -s `realpath .` ~/.zprezto
